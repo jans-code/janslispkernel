1a7805793e3b9ab697bdf24d4754f46f43a530e7
jans-code <7jantb@gmx.de>
2023-07-04 16:02:20 +0200

    changed versioning

--- src/janslispkernel/__init__.py
+++ src/janslispkernel/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # *_* coding: utf-8 *_*
 
-"""lisp kernel init"""
+"""Init module"""
 
-__version__ = "0.0.1"
+from importlib.metadata import version
+__version__ = version(__package__)
+
